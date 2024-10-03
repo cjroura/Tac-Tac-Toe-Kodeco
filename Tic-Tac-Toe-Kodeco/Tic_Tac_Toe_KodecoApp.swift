e25654561f470853b7f52230a32d07c603b39f13
cjroura <Christopher@Roura.Tech>
2024-10-03 19:54:08 -0400

    Initial Commit

---
+++ Tic-Tac-Toe-Kodeco/Tic_Tac_Toe_KodecoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_Toe_KodecoApp.swift
+//  Tic-Tac-Toe-Kodeco
+//
+//  Created by Christopher J. Roura on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_Toe_KodecoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
