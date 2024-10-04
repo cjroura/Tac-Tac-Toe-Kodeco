51b9a5abc376d5dc5c656b14b4a942de48a0922c
cjroura <Christopher@Roura.Tech>
2024-10-03 20:30:40 -0400

    [SETUP-01] Created Initial tab bar view
    - Created a tab view with dummy placeholders

--- Tic-Tac-Toe-Kodeco/ContentView.swift
+++ Tic-Tac-Toe-Kodeco/ContentView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            Text("First View")
+                .tabItem {
+                    Text("Play")
+                    Image(systemName: "gamecontroller")
+                }
+
+            Text("Second View")
+                .tabItem {
+                    Text("Saved games")
+                    Image(systemName: "bookmark")
+                }
         }
-        .padding()
     }
 }
 
